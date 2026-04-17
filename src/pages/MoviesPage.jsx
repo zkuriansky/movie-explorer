b9bfcf12f059812527eda96ff370820d22c174f2
Kurianskiy Zakhar <thezahpress@gmail.com>
2026-04-17 15:13:07 +0300

    Small refactoring

--- src/pages/MoviesPage.jsx
+++ src/pages/MoviesPage.jsx
@@ -1,7 +1,19 @@
-import Explorer from "../components/Explorer";
+import Filter from "../components/Filter";
+import Header from "../components/Header";
+import MovieList from "../components/MovieList";
+import Pagination from "../components/Pagination";
 
 const MoviesPage = () => {
-  return <Explorer />;
+  return (
+    <div className="container">
+      <Header />
+      <div className="main__app">
+        <Filter />
+        <MovieList />
+      </div>
+      <Pagination />
+    </div>
+  );
 };
 
 export default MoviesPage;
